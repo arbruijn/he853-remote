f3ebc8e471232622a8ee20ac354380ba475a0eba
Arne de Bruijn <ar112@pdc.nl>
2017-05-03 16:32:24 +0200

    Prevent crash on error

--- main.cpp
+++ main.cpp
@@ -4,9 +4,15 @@
 
 int main(int argc, char **argv)
 {
+	if (argc < 3) {
+		printf("%s: deviceId command\n", argv[0]);
+		return 1;
+	}
 	int deviceId = atoi(argv[1]);
 	int command = atoi(argv[2]);
 	HE853Controller *remote = new HE853Controller();
+	if (!remote->getDeviceStatus())
+		return 1;
 	printf("Sending command[%i] to deviceId[%i]\n", command, deviceId);
 	remote->sendEU((uint16_t)deviceId, (uint8_t)command);
 	return 0;
